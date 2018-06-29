3d65ae70e96132999735ad728129fe296d89b75e
elone <1052460303@qq.com>
2018-06-29 13:00:27 +0800

    first version

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'genpass',
+    version = '0.0.1',
+    py_modules = ['genpass'],
+    author = 'Y30lTo7S53',
+    author_email = 'uHAPflAovu@4Kz.com',
+    url = 'https://www.github.com',
+    description = 'anonymous',
+)
